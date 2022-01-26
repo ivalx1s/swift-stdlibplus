c72a31c13773705ff38ba01e68f3593d47adc5fc
Ivan O <ioparin@pm.me>
2022-01-26 14:38:16 +0300

    added predefined date formatters

---
+++ GalenIT.CoreSDK/Utils/Extensions/Formatters.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+// A set of cached predefenied formatters for convinient and efficient use from SwiftUI
+extension Formatter {
+    static var mediumDateStyle: DateFormatter = {
+        let formatter: DateFormatter = .init()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    static var shortDateStyle: DateFormatter = {
+        let formatter: DateFormatter = .init()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    static var longDateStyle: DateFormatter = {
+        let formatter: DateFormatter = .init()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    static var fullDateStyle: DateFormatter = {
+        let formatter: DateFormatter = .init()
+        formatter.dateStyle = .full
+        formatter.timeStyle = .none
+        return formatter
+    }()
+}
