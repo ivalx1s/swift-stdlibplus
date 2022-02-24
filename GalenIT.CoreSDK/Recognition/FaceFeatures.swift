b12a6ef279cbc056c0ad3a8fe31f17ab567cfba3
alexis <alexis@wildapricot.com>
2022-02-24 15:57:13 +0300

    face comparison

--- GalenIT.CoreSDK/Recognition/FaceFeatures.swift
+++ GalenIT.CoreSDK/Recognition/FaceFeatures.swift
@@ -13,3 +13,20 @@ public struct FaceFeatures {
     public let trackingFrameCount: Int?
     public let trackingID: Int?
 }
+
+public extension FaceFeatures {
+    static func build(_ feature: CIFaceFeature) -> FaceFeatures {
+        FaceFeatures(
+                faceBounds: feature.bounds,
+                leftEyeAt: feature.hasLeftEyePosition ? feature.leftEyePosition : nil,
+                leftEyeClosed: feature.hasLeftEyePosition ? feature.leftEyeClosed : nil,
+                rightEyeAt: feature.hasRightEyePosition ? feature.rightEyePosition : nil,
+                rightEyeClosed: feature.hasRightEyePosition ? feature.rightEyeClosed : nil,
+                faceAngle: feature.hasFaceAngle ? feature.faceAngle : nil,
+                mouthAt: feature.hasMouthPosition ? feature.mouthPosition : nil,
+                hasSmile: feature.hasMouthPosition,
+                trackingFrameCount: feature.hasTrackingFrameCount ? Int(feature.trackingFrameCount) : nil,
+                trackingID: feature.hasTrackingID ?  Int(feature.trackingID) : nil
+        )
+    }
+}
---
+++ GalenIT.CoreSDK/Recognition/FaceImageHelper.swift
@@ -0,0 +1,29 @@
+import Foundation
+import SwiftUI
+
+public struct FaceImageHelper {
+    
+    public static func squareImg(_ img: UIImage?) -> UIImage? {
+        guard let img = img else { return nil }
+
+        return img.croppedToSquare(
+                rect: FaceImageHelper.calcPhotoRect(for: img)
+        )
+    }
+
+    public static func calcPhotoRect(for photo: UIImage) -> CGRect {
+        switch photo.size.width > photo.size.height {
+        case true:
+            let rectWidth = photo.size.height
+            let rectOriginX = (photo.size.width - rectWidth) / 2
+
+            return .init(x: rectOriginX, y: 0, width: rectWidth, height: photo.size.height)
+
+        case false:
+            let rectHeight = photo.size.width
+            let rectOriginY = (photo.size.height - rectHeight) / 2
+
+            return .init(x: 0, y: rectOriginY, width: photo.size.width, height: rectHeight)
+        }
+    }
+}
