ba30aff0540c5a22d846d95ab978f0a01143a55e
Ivan O <ioparin@pm.me>
2022-06-22 22:11:22 +0400

    non-breaking change to delay(:) parameters

--- Sources/Utils/MainQueueUtils.swift
+++ Sources/Utils/MainQueueUtils.swift
@@ -5,8 +5,9 @@ import Foundation
 /// ❗️Mind the capture semantics; *action* is captured immediately on *delay* call.
 /// - parameter delayTime: time in seconds to wait until execution starts
 /// - parameter action: a block of code to execute
-public func delay(for delayTime: Double, action: @escaping  () -> Void) {
-    DispatchQueue.main.asyncAfter(deadline: .now() + delayTime) {
+public func delay(for delayTime: Double?, action: @escaping  () -> Void) {
+    let delay = delayTime ?? 0.0
+    DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
         action()
     }
 }
