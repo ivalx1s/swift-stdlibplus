178a7ed7ce5b669274e659bd429fc4984c4286b8
alexis <alexis@wildapricot.com>
2022-10-13 12:25:10 +0300

    adding task+delay

---
+++ Sources/Utils/Extensions/Task+Delay.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+public extension Task where Failure == Error {
+    @discardableResult
+    static func delayed(
+            byTimeInterval delayInterval: TimeInterval,
+            priority: TaskPriority? = nil,
+            operation: @escaping @Sendable () async throws -> Success
+    ) -> Task {
+        Task(priority: priority) {
+            let delay = UInt64(delayInterval * 1_000_000_000)
+            try await Task<Never, Never>.sleep(nanoseconds: delay)
+            return try await operation()
+        }
+    }
+}
