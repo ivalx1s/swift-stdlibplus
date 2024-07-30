2b6583385b334338f75ae36a953c73de71f9fa41
Ivan Oparin <i.oparin@unitea.com>
2024-07-30 21:11:03 +0400

    Remove foundation dependent extensions

--- Sources/Extensions/StringExt.swift
+++ Sources/Extensions/StringExt.swift
@@ -16,15 +16,6 @@ public extension StringProtocol where Self == String {
 	}
 }
 
-public extension String {
-    var removingWhitespaces: String {
-        components(separatedBy: .whitespaces).joined()
-    }
-    func replacingWhitespaces(with character: Character) -> Self {
-        components(separatedBy: .whitespaces).joined(separator: String(character))
-    }
-}
-
 public extension Optional where Wrapped == String {
     var isNilOrEmpty: Bool {
         self?.isEmpty ?? true
@@ -32,33 +23,9 @@ public extension Optional where Wrapped == String {
 }
 
 public extension String {
-    func cleanHtml() -> String {
-        self.replacingOccurrences(
-                of: "<[^>]+>",
-                with: "",
-                options: .regularExpression,
-                range: nil
-        )
-    }
-
-    func clean(pattern: String) -> String {
-        self.replacingOccurrences(
-                of: pattern,
-                with: "",
-                options: .regularExpression,
-                range: nil
-        )
-    }
-
-    var isNotEmpty: Bool {
-        !self.isEmpty
-    }
-
-    var isWhiteSpaceOrEmpty: Bool {
-        self
-            .trimmingCharacters(in: .whitespacesAndNewlines)
-            .isEmpty
-    }
+	var isNotEmpty: Bool {
+		!self.isEmpty
+	}
 }
 
 public extension String {
@@ -85,12 +52,5 @@ public extension String {
         let start = index(startIndex, offsetBy: range.lowerBound)
         let end = index(start, offsetBy: range.upperBound - range.lowerBound)
         return String(self[start ..< end])
-    }
-}
-
-public extension String {
-    var doubleVal: Double? {
-        let clear = self.replacingOccurrences(of: ",", with: ".")
-        return Double(clear)
     }
 }
