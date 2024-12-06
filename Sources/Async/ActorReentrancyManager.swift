a745c3e158425c766a300a90adaa6f66de6cd2a1
Ivan Oparin <oparin@me.com>
2024-12-07 03:51:38 +0400

    Conform actor reentrancy manager to sendable to support usage from Swift 6

--- Sources/Async/ActorReentrancyManager.swift
+++ Sources/Async/ActorReentrancyManager.swift
@@ -1,10 +1,11 @@
 
-public final class ActorReentrancyManager {
+public final class ActorReentrancyManager: @unchecked Sendable {
 	private let lock = AsyncLock()
 	
 	public init() {}
 	
-	public func performProtected<T>(_ work: () async throws -> T) async rethrows -> T {
+    @Sendable
+    public func performProtected<T>(_ work: @Sendable () async throws -> T) async rethrows -> T {
 		try await lock.withLock {
 			try await work()
 		}
